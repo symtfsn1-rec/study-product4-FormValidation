612b62c61adf9985d47ec6145a935332a77a1bfd
“snmrec” <symtfsn1@gmail.com>
2026-03-04 06:36:19 +0900

    step3: 入力イベントの監視と個別チェック&CSSの追加（視覚的な警告）

--- script.js
+++ script.js
@@ -9,3 +9,38 @@ const submitBtn = document.getElementById('submitBtn');
 // 2. バリデーション用ルール（正規表現）の定義
 // メールアドレスの形式チェック用パターン
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// 3. 名前入力のバリデーション関数
+const validationName = () => {
+  const value = usernameInput.value.trim();
+
+  if (value.length >= 3) {
+    nameError.textContent = '';
+    return true;
+  } else {
+    nameError.textContent = '名前は3文字以上で入力してください';
+    return true;
+  }
+};
+
+// 4. メール入力のバリデーション関数
+const validationEmail = () => {
+  const value = emailInput.value.trim();
+
+  if (emailPattern.test(value)) {
+    emailError.textContent = '';
+    return true;
+  } else {
+    emailError.textContent = '名前は3文字以上で入力してください';
+    return false;
+  }
+};
+
+// 5. 入力イベント（input）に紐付け
+usernameInput.addEventListener('input', () => {
+  validationName();
+});
+
+emailInput.addEventListener('input', () => {
+  validationEmail();
+});
